bda3056852520fc24929197a3a310d9c08b177a8
Abdurrahman Abdulaziz <abdurrahmanabdulaziz1515@gmail.com>
2019-12-10 22:36:20 +0200

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { AgmCoreModule } from "@agm/core";
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AgmCoreModule.forRoot({
+      apiKey: "AIzaSyA6Uy3lAf60fvCHHQaDHmXBHyGAd9VEH5w"
+    })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
